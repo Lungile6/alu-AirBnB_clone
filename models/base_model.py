0e74b3e8268537dc3c76aa5bd411e8ea76226aa5
Lungile6 <l.mabelebel@student.com>
2024-09-12 23:43:19 +0200

    The third tasks files co-operating as expected

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,33 @@
 #!/usr/bin/python3
 """
-Module for the BaseModel class.
+Base Model modue
 """
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+    def __init__(self):
+        """
+        Initializes a new BaseModel object with a unique id,
+        creation time, and update time.
+        """
         self.id = str(uuid.uuid4())
+
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
-        # Update attributes if kwargs are provided
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key in ["created_at", "updated_at"]:
-                    setattr(self, key, datetime.strptime(value, time_format))
-                else:
-                    setattr(self, key, value)
-
-        models.storage.new(self)
 
     def save(self):
-        """Update the 'updated_at' attribute and save the model"""
+        """
+        Updates the 'updated_at' attribute of
+        the BaseModel object to the current time.
+        """
         self.updated_at = datetime.utcnow()
-        models.storage.save()
 
     def to_dict(self):
-        """Return a dictionary representation of the model"""
+        """
+        Converts the BaseModel object into a dictionary for serialization.
+        """
         inst_dict = self.__dict__.copy()
         inst_dict["__class__"] = self.__class__.__name__
         inst_dict["created_at"] = self.created_at.isoformat()
@@ -40,20 +36,21 @@ class BaseModel:
         return inst_dict
 
     def __str__(self):
-        """Return a string representation of the model"""
+        """
+        Returns a string representation of the BaseModel object.
+        """
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
 
 if __name__ == "__main__":
-    # Test the BaseModel class
+    # Example code to demonstrate the usage of the BaseModel class
     my_model = BaseModel()
-    my_model.name = "My_First_Model"
+    my_model.name = "My First Model"
     my_model.my_number = 89
-    print(my_model.id)
     print(my_model)
-    print(type(my_model.created_at))
-    print("--")
+    my_model.save()
+    print(my_model)
     my_model_json = my_model.to_dict()
     print(my_model_json)
     print("JSON of my_model:")
@@ -61,12 +58,3 @@ if __name__ == "__main__":
         print("\t{}: ({}) - {}".format(
             key, type(my_model_json[key]), my_model_json[key])
             )
-
-    print("--")
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model.id)
-    print(my_new_model)
-    print(type(my_new_model.created_at))
-
-    print("--")
-    print(my_model is my_new_model)
